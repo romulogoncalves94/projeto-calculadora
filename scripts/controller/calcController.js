18921c3face3cb807ed6ea08f8d6fd99b210315a
Romulo Vinicius Leme Goncalves <romulo.goncalves@triersistemas.corp>
2022-03-02 15:36:01 -0300

    configuração dos metodos de data e hora

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,25 +2,58 @@ class CalcController {
     
     constructor(){
 
-        this._displayCalc = "0";
-        this._dataAtual;
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl =document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
 
     }
 
-//Recuperar valor
+    initialize (){
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
+    }
+
+    setDisplayDateTime (){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+    
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
     get displayCalc(){
-        return this._displayCalc;
-    }
-//Atribuir valor
-    set displayCalc(valor){
-        this._displayCalc = valor;
+        return this._displayCalcEl.innerHTML;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
-    set dataAtual(valor){
-        this._displayCalc = valor;
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._displayCalc = value;
     }
 }
