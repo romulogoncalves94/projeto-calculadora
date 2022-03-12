a40115b0151eb34f0c8605045a41575a932d7f9f
Rômulo Gonçalves <romulogoncalves94@hotmail.com>
2022-03-12 09:44:57 -0300

    botão ponto e correção de bugs do botão ponto

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -43,6 +43,10 @@ class CalcController {
 
         this._operation = [];
 
+        this._lastNumber = '';
+
+        this._lastOperator = '';
+
         this.setLastNumberToDisplay();
 
     }
@@ -177,16 +181,11 @@ class CalcController {
 
     addOperation(value){
 
-
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
 
                 this.setLastOperation(value);
-
-            } else if (isNaN(value)){
-
-                console.log("outra coisa",value);
 
             } else {
 
@@ -206,7 +205,7 @@ class CalcController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -220,6 +219,26 @@ class CalcController {
 
         this.displayCalc = "Error";
         
+    }
+
+    addDot(){
+
+        let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
+        if (this.isOperator(lastOperation) || !lastOperation){
+            
+            this.pushOperation('0.');
+       
+        } else {
+           
+            this.setLastOperation(lastOperation.toString() + '.');
+        
+        }
+
+        this.setLastNumberToDisplay();
+
     }
 
     execBtn(value){
@@ -259,7 +278,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
                 break;
 
             case '0':
