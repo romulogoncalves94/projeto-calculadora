b93d11df8ae8fda1d7e56e52d2836298d3e218bb
Rômulo Gonçalves <romulogoncalves94@hotmail.com>
2022-03-12 10:33:42 -0300

    definido valor máx. de caracteres

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -85,7 +85,7 @@ class CalcController {
             this._audio.currentTime = 0;
 
             this._audio.play();
-            
+
         }
 
     }
@@ -488,6 +488,13 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if (value.toString().length > 10){
+            
+            this.setError();
+            
+            return false;
+        }
 
         this._displayCalcEl.innerHTML = value;
 
